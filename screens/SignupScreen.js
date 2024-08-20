543aa08c82c30c84c25ea2cec2b842174e3b8766
Moses-amos <gamasebmoses2@gmail.com>
2024-08-20 16:42:20 +0200

    font pending

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -11,12 +11,6 @@ export default function SignupScreen() {
     <View className="bg-white h-full w-full">
         <StatusBar style="light" />
         <Image className="h-full w-full absolute" source={require('../assets/background.jpg')} />
-        
-        {/* Lights */}
-        {/* <View className="flex-row justify-around w-full absolute">
-            <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify()} className="h-[225] w-[90]" source={require('../assets/light.png')} />
-            <Animated.Image entering={FadeInUp.delay(400).duration(1000).springify()} className="h-[160] w-[65]" source={require('../assets/light.png')} />
-        </View> */}
 
         {/* Title and form */}
         <View className='h-full w-full justify-around pt-40'>
