f8a6e4f76f7f7078bd9beec8268c30299017c98f
Moses-amos <gamasebmoses2@gmail.com>
2024-08-20 16:03:49 +0200

    initial

---
+++ screens/SignupScreen.js
@@ -0,0 +1,62 @@
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import Animated, { FadeInUp, FadeInDown } from 'react-native-reanimated'
+import { useNavigation } from '@react-navigation/native'
+
+export default function SignupScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View className="bg-white h-full w-full">
+        <StatusBar style="light" />
+        <Image className="h-full w-full absolute" source={require('../assets/background.jpg')} />
+        
+        {/* Lights */}
+        {/* <View className="flex-row justify-around w-full absolute">
+            <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify()} className="h-[225] w-[90]" source={require('../assets/light.png')} />
+            <Animated.Image entering={FadeInUp.delay(400).duration(1000).springify()} className="h-[160] w-[65]" source={require('../assets/light.png')} />
+        </View> */}
+
+        {/* Title and form */}
+        <View className='h-full w-full justify-around pt-40'>
+         {/* Title */}
+          <View className='flex items-center'>
+          <Animated.Text entering={FadeInUp.duration(1000).springify()} className='text-white font-bold tracking-wider text-5xl'>
+            Sign Up
+          </Animated.Text>
+          </View>
+
+          {/* form */}
+          <View className='flex items-center mx-4 pt-10 space-y-4'>
+           <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full">
+          <TextInput placeholder='Email' placeholderTextColor={'gray'} />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full">
+          <TextInput placeholder='Enter your password' placeholderTextColor={'gray'} />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full mb-3">
+          <TextInput placeholder='Confirm Password' placeholderTextColor={'gray'} secureTextEntry />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.duration(1000).springify()} className="w-full">
+            <TouchableOpacity 
+            className="w-full bg-sky-600 p-3 rounded-2xl mb-3">
+              <Text className="text-xl font-bold text-white text-center">Sign Up</Text>
+            </TouchableOpacity>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.duration(1000).springify()} className="flex-row justify-center">
+            <Text>Already have an account? </Text>
+            <TouchableOpacity onPress={() => navigation.push('login')}>
+              <Text className="text-sky-600 font-semibold ">login</Text>
+            </TouchableOpacity>
+          </Animated.View>
+          </View>
+          
+
+        </View>
+
+
+
+    </View>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@designbycode/tailwindcss-text-shadow")
+    ({
+      shadowColor: "rgba(0, 0, 0, 0.5)",
+      shadowBlur: "3px",
+      shadowOffsetX: "2px",
+      shadowOffsetY: "2px",
+  }),
+  ],
+}
+
